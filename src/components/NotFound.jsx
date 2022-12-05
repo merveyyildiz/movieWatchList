6924406a6435887bda8710bfc2022fbcfa7b981e
Merve Yıldız <merveyildizzz41@gmail.com>
2022-12-05 14:02:29 +0300

    404 sayfasına helmet eklendi

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Helmet from './Helmet'
 
 const NotFound = () => {
   return (
-    <p className="not-found">This page is not found.</p>
+    <Helmet title="Not Found">
+        <p className="not-found">This page is not found.</p>
+    </Helmet>
+
   )
 }
 
