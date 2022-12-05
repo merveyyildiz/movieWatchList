e278834745e65fff0a25dde1e895ae22e3af4c12
Merve Yıldız <merveyildizzz41@gmail.com>
2022-12-05 14:18:45 +0300

    style değiştirildi

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import "../App.scss";
 import MovieIcons from "./MovieIcons";
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 const Card = ({movie, type}) => {
+    const {setMovie} = useContext(GlobalContext);
 
+    const navigator = useNavigate();
+
+    const handleClick = () => {
+        setMovie(movie);
+        navigator(`/movie/${movie.title}`);
+    }
+    
     return (
         <div className="card">
-            <img className="card_img" src={movie.poster_path ? `https://www.themoviedb.org/t/p/w440_and_h660_face${movie.poster_path}` : './assets/images/placeholder.jpg'} alt="poster"/>
+            <img className="card_img" onClick={handleClick} src={movie.poster_path ? `https://www.themoviedb.org/t/p/w440_and_h660_face${movie.poster_path}` : './assets/images/placeholder.jpg'} alt="poster"/>
             <MovieIcons movie={movie} type={type} />
         </div>
     )
